599e9290bb293578695c93246b882980305cb3c1
diana-sanchez-de-la.cruz <16470289@itcampeche.edu.mx>
2017-06-03 19:55:06 -0500

    Tercera version del proyecto.

--- src/modelo/Empleado.java
+++ src/modelo/Empleado.java
@@ -9,6 +9,75 @@ package modelo;
  *
  * @author Sanchez de la Cruz
  */
-public class Empleado {
+public abstract class Empleado {
+    private String primerNombre;
+    private String apellidoPaterno;
+    private String numeroSeguroSocial;
     
+    public Empleado (String nombre,String apellido, String nss){
+        primerNombre=nombre;
+        apellidoPaterno=apellido;
+        numeroSeguroSocial=nss;
+    }
+    
+    public void establecerPrimerNombre (String nombre){
+        primerNombre= nombre;
+    }
+    public String ObtenerPrimerNombre (){
+        return primerNombre;
+    }
+       public void establecerApellidoPaterno (String apellido){
+        apellidoPaterno= apellido;
+    }
+    public String ObtenerApellidoPaterno (){
+        return apellidoPaterno;
+    }
+       public void establecerNumeroSeguroSocial (String nss){
+       numeroSeguroSocial= nss;
+    }
+    public String ObtenerNumeroSeguroSocial (){
+        return numeroSeguroSocial;
+    }
+    @Override
+    public String toString(){
+     return String.format("%s %s/numero de seguro social: %s",
+   ObtenerPrimerNombre(),ObtenerApellidoPaterno(),ObtenerNumeroSeguroSocial() );
+    }
+    public abstract double ingresos ();
+
+
+
+
+public class EmpleadoAsalariado extends Empleado  {
+   
+private double salarioSemanal;
+
+public EmpleadoAsalariado  (String nombre,String apellido, String nss, 
+        double salario ){
+    
+    super (nombre, apellido, nss);
+    establecerSalarioSemanal (salario);
 }
+public void establecerSalarioSemanal (double salario){
+    if (salario >= 0.0)
+        salarioSemanal=salario;
+    else 
+        throw new IllegalArgumentException (
+        "El salario semenal deber ser >= 0.0");
+}
+public double obtenerSalarioSemanal (){
+    return salarioSemanal;
+}
+@Override 
+public double ingresos (){
+    return obtenerSalarioSemanal();
+}
+@Override 
+public String toString (){
+    return String.format("empelado asalariado:%s/n%s:$%,.2f",
+            super.toString(),"salario semanal", obtenerSalarioSemanal()); 
+}
+}
+}
+
+
--- src/modelo/EmpleadoAsalariado.java
+++ src/modelo/EmpleadoAsalariado.java
@@ -9,6 +9,33 @@ package modelo;
  *
  * @author Sanchez de la Cruz
  */
-public class EmpleadoAsalariado {
+public class EmpleadoAsalariado extends Empleado  {
+   
+private double salarioSemanal;
+
+public EmpleadoAsalariado  (String nombre,String apellido, String nss, 
+        double salario ){
     
+    super (nombre, apellido, nss);
+    establecerSalarioSemanal (salario);
+}
+public void establecerSalarioSemanal (double salario){
+    if (salario >= 0.0)
+        salarioSemanal=salario;
+    else 
+        throw new IllegalArgumentException (
+        "El salario semenal deber ser >= 0.0");
+}
+public double obtenerSalarioSemanal (){
+    return salarioSemanal;
+}
+@Override 
+public double ingresos (){
+    return obtenerSalarioSemanal();
+}
+@Override 
+public String toString (){
+    return String.format("empelado asalariado:%s/n%s:$%,.2f",
+            super.toString(),"salario semanal", obtenerSalarioSemanal()); 
+}
 }
